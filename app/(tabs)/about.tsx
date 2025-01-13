729fd70da23552b897b5c0693cbcbf7cac3c283f
WillC <cooperw@sonoma.edu>
2025-01-13 00:25:24 -0800

    Initial stuff, camera sort of working

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,22 @@
+import {Text, View, StyleSheet} from "react-native";
+
+export default function About() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Placeholder</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#fff',
+
+    },
+});
